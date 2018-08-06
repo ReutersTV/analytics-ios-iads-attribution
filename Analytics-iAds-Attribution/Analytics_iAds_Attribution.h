9081ec6b746aa224b243996d678ede9c6768e2f4
marcvanolmen <marcvanolmen@gmail.com>
2018-08-05 23:01:08 -0400

    [add] carthage project.

---
+++ Analytics-iAds-Attribution/Analytics_iAds_Attribution.h
@@ -0,0 +1,19 @@
+//
+//  Analytics_iAds_Attribution.h
+//  Analytics-iAds-Attribution
+//
+//  Created by Marc Van Olmen on 8/5/18.
+//  Copyright © 2018 Thomson Reuters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Analytics_iAds_Attribution.
+FOUNDATION_EXPORT double Analytics_iAds_AttributionVersionNumber;
+
+//! Project version string for Analytics_iAds_Attribution.
+FOUNDATION_EXPORT const unsigned char Analytics_iAds_AttributionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Analytics_iAds_Attribution/PublicHeader.h>
+
+
